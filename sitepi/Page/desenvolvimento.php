92fa8a2ca87573fc7530e65751dfd452c7ba49d4
calouros <calouros@fatec.corp>
2025-06-17 10:23:32 -0300

    commit

---
+++ sitepi/Page/desenvolvimento.php
@@ -0,0 +1,4 @@
+Mediante ao desenvolvimento do Projeto Integrador, podemos detalhar que a 
+Plataforma Educacional de Programação em Python e Línguagem em Inglês para o público infantojuvenil na daixa etária de 7 a 14 anos 
+contêm diversos pontos de relevância, tais como:
+Fusão da linguagem python e o ensino em inglês, 
